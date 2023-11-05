eb33eee14dc0f6b0a7da00e454aee464ca0b8a06
Drew <github@agoristhosting.com>
2023-11-04 21:05:39 -0700

    compiling and running in studio.

--- data_structures_employee.c
+++ data_structures_employee.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -5,29 +7,32 @@
 
 typedef struct {
 
-	char firstName[20];
+    char firstName[20];
     char lastName[20];
+    int gender;
     int ID;
     double yearsAt;
     int salary;
 
-} employeeStruct ;
+} employeeStruct;
 
 int main()
 {
-    employeeStruct emp[5];
+    employeeStruct emp[6];
     int i;
     int numEmps = 2;
 
     printf("enter in info for 4 employees...\n");
     printf("First Name, Last Name, ID Number, Years Emplyed, Salary.\n");
-    for (i =0; i < numEmps; i++) {
+    for (i = 0; i < numEmps; i++) {
 
         printf("\tFirst Name \n\t");
         scanf(" %s", emp[i].firstName);
         printf("\tLast Name \n\t");
         scanf(" %s", emp[i].lastName);
-        printf("\tEmployee ID number \n\t");Sy
+        printf("\tEnter 1 for male 2 for female: \n\t");
+        scanf("%d", &emp[i].gender);
+        printf("\tEmployee ID number \n\t");
         scanf("%d", &emp[i].ID);
         printf("\tYears at this complany \n\t");
         scanf("%lf", &emp[i].yearsAt);
@@ -35,15 +40,23 @@ int main()
         scanf("%d", &emp[i].salary);
 
     }
-    
-    for (i=0; i < numEmps; i++) {
+
+    for (i = 0; i < numEmps; i++) {
         printf("\n");
-        printf("Unepic Networks Employee number %d\n", i+1);
+        printf("Unepic Networks Employee number %d\n", i + 1);
         printf("\t%s\n", emp[i].firstName);
         printf("\t%s\n", emp[i].lastName);
         printf("\tID Number %d\n", emp[i].ID);
-        printf("\t%s has been working here %.2lf years\n", emp[i].firstName, emp[i].yearsAt);
-        printf("\tAnd we pay him/her $%d\n", emp[i].salary);
+        printf("\t%s has been working here %.1lf years\n", emp[i].firstName, emp[i].yearsAt, emp[i].yearsAt == 1 ? "" : "s");
+
+        if (emp[i].gender == 1) {
+            printf("\tAnd we pay him $%d\n", emp[i].salary);
+        }
+
+        else if (emp[i].gender == 2) {
+            printf("\tAnd we pay her $%d\n", emp[i].salary);
+        }
+
     }
 
     return 0;
